e3f75b379b4300d9a49b4e8d86eed5bb232726ae
dyankov <dyankov.nikolay@gmail.com>
2015-12-25 20:41:03 +0200

    functionality done

--- magicScroll.js
+++ magicScroll.js
@@ -1,14 +1,46 @@
-// Options: { scrollRange: [], domain: [], onUpdate: function() {}, onComplete: function() {} }
 function MagicScroll(options) {
+    var self = this;
+
+    var defaults = {
+        scrollRange: [0, 0],
+        domain: [0, 0],
+        onStart: function() {},
+        onUpdate: function() {},
+        onComplete: function() {},
+    }
+    options = $.extend(defaults, true, options);
+
+    self.started = false;
+    self.completed = false;
     options.onUpdate(options.domain[0]);
 
     $(window).on('scroll', function() {
         var scrollTop = $(window).scrollTop();
 
         if (scrollTop >= options.scrollRange[0] && scrollTop <= options.scrollRange[1]) {
+            if (!self.started) {
+                options.onStart();
+            }
+
             var progress = (scrollTop - options.scrollRange[0]) / (options.scrollRange[1] - options.scrollRange[0]);
             var v = options.domain[0] + progress * (options.domain[1] - options.domain[0]);
             options.onUpdate(v);
+        }
+
+        if (scrollTop < options.scrollRange[0]) {
+            var progress = 0;
+            var v = options.domain[0] + progress * (options.domain[1] - options.domain[0]);
+            options.onUpdate(v);
+        }
+
+        if (scrollTop > options.scrollRange[1]) {
+            var progress = 1;
+            var v = options.domain[0] + progress * (options.domain[1] - options.domain[0]);
+            options.onUpdate(v);
+
+            if (!self.completed) {
+                options.onComplete();
+            }
         }
 
         if (options.debug) {
